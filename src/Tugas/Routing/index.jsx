a54370b1ff66fd5c9e230febb4d1e9e4efe3c072
bashofi54 <bashofi54@gmail.com>
2022-08-02 23:10:38 +0700

    Tugas ROUTING

--- src/Tugas/Routing/index.jsx
+++ src/Tugas/Routing/index.jsx
@@ -4,7 +4,7 @@ const Routee = () => {
             <div className="App" >
                 <img src="https://eduwork.id/images/logo3.png" alt="lgo"></img>
                 <hr />
-                <h1>kumpulan tugas tugas MERN</h1>
+                <h1><strong>kumpulan tugas tugas MERN</strong></h1>
             </div>
         </div>
     )
